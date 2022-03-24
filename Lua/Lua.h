9db14a3cea84625b9346a6b2fbc369c6eaeb89d0
zhkl0228 <zhkl0228@qq.com>
2022-03-24 17:54:06 +0800

    Initial Commit

---
+++ Lua/Lua.h
@@ -0,0 +1,18 @@
+//
+//  Lua.h
+//  Lua
+//
+//  Created by Banny on 2022/3/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Lua.
+FOUNDATION_EXPORT double LuaVersionNumber;
+
+//! Project version string for Lua.
+FOUNDATION_EXPORT const unsigned char LuaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lua/PublicHeader.h>
+
+
